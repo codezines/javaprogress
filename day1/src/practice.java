df7099cecede1bb4c8586ad00a685d82659ac50b
priya <priyadeveloper434@gmail.com>
2023-10-18 12:17:31 +0530

    started

--- day1/src/practice.java
+++ day1/src/practice.java
@@ -1,2 +1,13 @@
 public class practice {
+    public static void main(String args[]){
+        //sum of elements in array
+        int i;
+        int [] a={1,5,3,6,2};
+        int sum=0;
+        for(i=0;i<a.length;i++){
+            System.out.println(a[i]);
+            sum=sum+a[i];
+        }
+        System.out.println("the sum is"+ sum);
+    }
 }
